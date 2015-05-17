afed43a8f024cdda01dd5233bd2f55e994e86148
violinisms <sakumo_whitefang@hotmail.com>
2015-05-17 14:27:54 +0700

    fix library bugs

--- app/src/main/java/me/outcube/cashsavior/MainActivity.java
+++ app/src/main/java/me/outcube/cashsavior/MainActivity.java
@@ -1,11 +1,19 @@
 package me.outcube.cashsavior;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.Signature;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.content.pm.PackageManager;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,6 +22,24 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//      find hash key
+        try {
+
+               PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+
+               for (Signature signature : info.signatures)
+               {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.i("Mint", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+               }
+
+              } catch (PackageManager.NameNotFoundException e) {
+               Log.e("name not found", e.toString());
+              } catch (NoSuchAlgorithmException e) {
+               Log.e("no such an algorithm", e.toString());
+        }
+//      find hash key
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
